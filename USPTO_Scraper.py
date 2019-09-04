f1918d83a2d3a96d0b4e0df27a011e1e6aab41ba
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2019-09-04 16:56:40 -0400

    Update USPTO_Scraper.py

--- USPTO_Scraper.py
+++ USPTO_Scraper.py
@@ -4,7 +4,7 @@ sys.path.append('/anaconda3/lib/python3.7/site-packages')
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("https://economics.nd.edu/faculty/rudiger-bachmann/")
+html = urlopen("url")
 
 bsObj = BeautifulSoup(html.read())
 print(bsObj.h1)
