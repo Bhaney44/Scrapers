fbcace53d0e407205a19b3a4e87598044c0e4d1f
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2020-02-14 17:16:32 -0500

    Add files via upload

--- Data_14.py
+++ Data_14.py
@@ -8,7 +8,7 @@ import csv
 #Write to Database
 def write():
     #Link to webpage with patent
-    html = urlopen("http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF&p=1&u=%2Fnetahtml%2FPTO%2Fsearch-bool.html&r=1&f=G&l=50&co1=AND&d=PTXT&s1=(%22deep+learning%22.CLTX.+or+%22deep+learning%22.DCTX.)&OS=ACLM/%22deep+learning%22&RS=ACLM/%22deep+learning%22")
+    html = urlopen("LINK")
 
     #Soup object
     soup = BeautifulSoup(html, 'html.parser')
