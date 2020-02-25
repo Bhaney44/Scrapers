ae6048371c4d31403ee81aa0bacf7dc969395ea2
B Seamus Haney <43055154+Bhaney44@users.noreply.github.com>
2020-02-25 05:19:43 -0500

    Add files via upload

--- Links_23.py
+++ Links_23.py
@@ -18,9 +18,6 @@ def get_title():
 
 get_title()
 
-####
-##I want to find a link on the page. This returns the correct link.
-####
 
 #Get
 def get_link():
@@ -30,16 +27,13 @@ def get_link():
     #Soup object
     soup = BeautifulSoup(html, 'html.parser')
     #Find image
-    ##image = <img valign="MIDDLE" src="/netaicon/PTO/nextdoc.gif" border="0" alt="[NEXT_DOC]">
-    #image = soup.find("img", valign="MIDDLE")
     image = soup.find("img", valign="MIDDLE", alt="[NEXT_DOC]")
     #Follow link
     ##link = image.parent
     link = image.parent
     new_link = link.attrs['href']
-    #new_url = 'http://patft.uspto.gov/'+new_link
-    #new_page = urlopen(new_url)
-    new_page = urlopen('http://patft.uspto.gov/'+new_link)
+    #Insert initial part of link
+    new_page = urlopen('"LINK"'+new_link)
     soup = BeautifulSoup(new_page, 'html.parser')
     #Patent Number
     Patent_Number = soup.title.text
@@ -48,7 +42,5 @@ def get_link():
 get_link()
 
     
-#Print Out
-##United States Patent: 10530579
-##
+
 
